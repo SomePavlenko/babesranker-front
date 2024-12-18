5228259ede148a75352185aebaa579bc962905d2
Павленко Александр Андреевич <aa.pavlenko@globus-it.com>
2024-12-18 17:53:33 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1
+          className="text-3xl font-bold underline"
         >
           Learn React
-        </a>
+        </h1>
       </header>
     </div>
   );
