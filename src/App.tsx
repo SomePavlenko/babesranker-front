ab2b8cc9e8f36604d84c6c07e0bfc8e8d0df73b2
Павленко Александр Андреевич <aa.pavlenko@globus-it.com>
2024-12-18 19:49:42 +0300

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1
-          className="text-3xl font-bold underline"
-        >
-          Learn React
-        </h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 className='text-3xl font-bold underline'>1 Learn React</h1>
+			<h1 className='text-2xl'>1 Learn React</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
