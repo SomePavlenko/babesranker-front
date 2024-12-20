3e9dc3f313584b38e21c8c7c59a21aa8f2ccb3e1
Павленко Александр Андреевич <aa.pavlenko@globus-it.com>
2024-12-20 15:08:48 +0300

    add header text

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 export const Header = () => {
 	return (
-		<div className='h-34 w-full overflow-hidden relative'>
+		<div className='h-34 w-full overflow-hidden relative z-20'>
 			<img
 				src={`${process.env.PUBLIC_URL}/images/banner.png`}
 				alt='banner'
 				className='w-full h-full object-cover object-center'
 			/>
-			Babes ranker
+			<p className='z-30 absolute top-1/2 left-1/4  text-4xl font-bold text-white'>
+				Babes ranker
+			</p>
 		</div>
 	)
 }
